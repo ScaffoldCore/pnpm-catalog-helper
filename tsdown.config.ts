cf581e2392ed168826066e8ca1bf82534487c633
lonewolfyx <877085893@qq.com>
2025-12-15 22:21:06 +0800

    style(tsdown.config): formats code spacing and commas

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'tsdown'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig([
     {
@@ -6,5 +6,5 @@ export default defineConfig([
         dts: true,
         clean: true,
         sourcemap: true,
-    }
+    },
 ])
