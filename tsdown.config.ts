8ebb7af2c69366f0f38f9385c34bb89a0c236848
lonewolfyx <877085893@qq.com>
2025-12-15 11:56:14 +0800

    Initial Project

---
+++ tsdown.config.ts
@@ -0,0 +1,10 @@
+import {defineConfig} from 'tsdown'
+
+export default defineConfig([
+    {
+        entry: 'src/cli.ts',
+        dts: true,
+        clean: true,
+        sourcemap: true,
+    }
+])
